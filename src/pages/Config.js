67201fa33ec66f0d8ce852ef642167c4aa7ec45b
s4bot4 <sabotamail@gmail.com>
2021-11-25 16:53:30 -0300

    Fix push main-group-16-Req03

--- src/pages/Config.js
+++ src/pages/Config.js
@@ -4,7 +4,7 @@ export default class Config extends Component {
   render() {
     return (
       <div>
-        <h1 data-testid="settings-title">Configuração</h1>
+        <h1 data-testid="settings-title">Configurações</h1>
       </div>
     );
   }
