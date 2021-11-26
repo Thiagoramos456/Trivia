bb6cd33f2a976331eacaf7887e96daeea2c16b3e
s4bot4 <sabotamail@gmail.com>
2021-11-26 18:54:12 -0300

    Requisito 14 Concluído

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -6,10 +6,22 @@ export default class Feedback extends Component {
     super();
 
     this.getFeedbackMessage = this.getFeedbackMessage.bind(this);
+    this.getScore = this.getScore.bind(this);
+    this.getAssertions = this.getAssertions.bind(this);
+  }
+
+  getScore() {
+    const { player: { score } } = JSON.parse(localStorage.getItem('state'));
+    return score;
+  }
+
+  getAssertions() {
+    const { player: { assertions } } = JSON.parse(localStorage.getItem('state'));
+    return assertions;
   }
 
   getFeedbackMessage() {
-    const { player: { assertions } } = JSON.parse(localStorage.getItem('state'));
+    const assertions = this.getAssertions();
     const MIN_ASSERTIONS = 3;
     return assertions < MIN_ASSERTIONS ? 'Podia ser melhor...' : 'Mandou bem!';
   }
@@ -22,6 +34,12 @@ export default class Feedback extends Component {
           {' '}
           {this.getFeedbackMessage()}
           {' '}
+        </h2>
+        <h2 data-testid="feedback-total-score">{`${this.getScore()}`}</h2>
+        <h2
+          data-testid="feedback-total-question"
+        >
+          {`${this.getAssertions()}`}
         </h2>
       </div>
     );
