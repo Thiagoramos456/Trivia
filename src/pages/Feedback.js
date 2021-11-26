c3ffdfa4f17e0a18127b27dfacb5b6bbc0817101
Arthur Bernardo Messias <turmessias@gmail.com>
2021-11-26 18:05:18 -0300

    Requisito 13 Concluido

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Feedback extends Component {
+  constructor() {
+    super();
+
+    this.getFeedbackMessage = this.getFeedbackMessage.bind(this);
+  }
+
+  getFeedbackMessage() {
+    const { player: { assertions } } = JSON.parse(localStorage.getItem('state'));
+    const MIN_ASSERTIONS = 3;
+    return assertions < MIN_ASSERTIONS ? 'Podia ser melhor...' : 'Mandou bem!';
+  }
+
   render() {
     return (
       <div>
         <Header />
-        <p data-testid="feedback-text">lalala</p>
+        <h2 data-testid="feedback-text">
+          {' '}
+          {this.getFeedbackMessage()}
+          {' '}
+        </h2>
       </div>
     );
   }
