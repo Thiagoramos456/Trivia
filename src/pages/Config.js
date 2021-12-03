5c6e8ae8365baeffede58b06d102b2cdd3934f7e
s4bot4 <sabotamail@gmail.com>
2021-12-03 14:54:00 -0300

    Implemntado background color na página Config

--- src/pages/Config.js
+++ src/pages/Config.js
@@ -4,7 +4,7 @@ export default class Config extends Component {
   render() {
     return (
       <div>
-        <h1 data-testid="settings-title">Configurações</h1>
+        <h1 className="text-center bg-info py-3" data-testid="settings-title">Configurações</h1>
       </div>
     );
   }
