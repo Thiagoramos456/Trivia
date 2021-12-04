6148a753a52e269da4d8ffd47a1da17398c28e1f
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-12-04 19:20:44 -0300

    Criando indicadores de progresso das questoes

--- src/redux/reducers/gameDataReducer.js
+++ src/redux/reducers/gameDataReducer.js
@@ -1,5 +1,12 @@
-import { FETCH_API_SUCCESS, FETCH_API_FAILED, LOADING,
-  PLAYER, RESET_PLAYER } from '../actions';
+/* eslint-disable max-lines-per-function */
+import {
+  FETCH_API_SUCCESS,
+  FETCH_API_FAILED,
+  LOADING,
+  PLAYER,
+  RESET_PLAYER,
+  ANSWER_QUESTION,
+} from '../actions';
 
 const INITIAL_STATE = {
   data: [],
@@ -11,6 +18,7 @@ const INITIAL_STATE = {
     gravatarEmail: '',
   },
   isLoading: false,
+  questionsProgress: [],
 };
 
 const gameDataReducer = (state = INITIAL_STATE, action) => {
@@ -20,13 +28,21 @@ const gameDataReducer = (state = INITIAL_STATE, action) => {
       ...state,
       isLoading: action.payload,
     };
-  case FETCH_API_SUCCESS:
+  case FETCH_API_SUCCESS: {
+    const questions = action.payload;
+    console.log(questions);
+    const questionsProgress = questions.map(({ question }) => ({
+      answered: false,
+      isCorrect: null,
+      question,
+    }));
     return {
       ...state,
-      data: action.payload,
+      data: questions,
       isLoading: false,
+      questionsProgress,
     };
-
+  }
   case FETCH_API_FAILED:
     return {
       ...state,
@@ -39,7 +55,20 @@ const gameDataReducer = (state = INITIAL_STATE, action) => {
       ...state,
       player: action.payload,
     };
+  case ANSWER_QUESTION: {
+    const { questionsProgress } = state;
+    const { qIndex, isCorrect } = action.payload;
+    const updatedQuestionsProgress = [...questionsProgress];
 
+    updatedQuestionsProgress[qIndex].answered = true;
+    updatedQuestionsProgress[qIndex].isCorrect = isCorrect;
+    console.log(updatedQuestionsProgress);
+
+    return {
+      ...state,
+      questionsProgress: updatedQuestionsProgress,
+    };
+  }
   case RESET_PLAYER:
     return {
       ...state,
